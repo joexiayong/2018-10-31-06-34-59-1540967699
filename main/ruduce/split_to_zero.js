1b1187ad3c6b13b302e2b706c5773258269ecc1d
Hia's <joexiayong@foxmail.com>
2019-04-12 22:14:12 +0800

    finished ruduce

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -2,6 +2,13 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  let arr = [];
+  arr.push(number);
+  while (number > 0) {
+    number = Number((number - interval).toFixed(1));
+    arr.push(number);
+  }
+  return arr;
 }
 
 module.exports = spilt_to_zero;
