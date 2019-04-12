d9b2b1d81a2278aab1ebf22fb1ec3a41db5aac98
Hia's <joexiayong@foxmail.com>
2019-04-12 22:35:28 +0800

    finished section_1

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,5 +1,8 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.filter(num=>{
+    return collection_b.includes(num)
+  })
 }
 
 module.exports = collect_same_elements;
--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,7 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  return collection_a.filter((element) => collection_b[0].includes(element))
+
 }
 
 module.exports = collect_same_elements;
